ea7ff9a77ba309f134544e16f3909fff091a76e2
TimAryavong <59714677+TimAryavong@users.noreply.github.com>
2020-07-01 14:18:01 -0400

    Update comments

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ Description: A to-do list that focuses on demonstrating event handlers and DOM m
 
 let list = document.querySelector('ol'); // bind ol element
 let submit = document.querySelector('.submit'); // bind submit button
-let remove = document.querySelector('.remove'); // bind submit button
+let remove = document.querySelector('.remove'); // bind delete button
 
 function addItem() {
     let checkbox = document.createElement('input'); // create input
@@ -21,7 +21,7 @@ function addItem() {
     document.getElementById('inputItem').value = ''; // clear the input field
 }
 
-function removeItem(e) { //add alert box that asks if you really want to delete the items.
+function removeItem(e) { // add alert box that asks if you really want to delete the items.
     if(confirm('Are you sure you want to delete?')){ // pop up confirmation for deletion
         let checkedBoxes = document.querySelectorAll('input'); // find all inputs
         let counter = 0;
@@ -40,7 +40,7 @@ submit.addEventListener('click', addItem); // listen for submit clicks
 remove.addEventListener('click', removeItem); // listen for delete clicks
 
 list.onchange = function (event) { // if checkbox is checked it will bubble to ol
-    if (event.target.checked == true) { //if the box is checked line-through the text
+    if (event.target.checked == true) { // if the box is checked line-through the text
         event.target.parentNode.style.textDecorationLine = 'line-through'; // change targeted item css to line-through
         list.appendChild(event.target.parentNode); // appends the targeted node to the bottom of the list
     } else { // else remove the line-through
@@ -49,7 +49,7 @@ list.onchange = function (event) { // if checkbox is checked it will bubble to o
 };
 
 window.onkeydown = function (e) { // adds enter key functionality to add items faster for testing
-    if (e.key == 'Enter') //key functions in place of keyCode, cool
+    if (e.key == 'Enter') // key (checks the name of the key) in place of keyCode (checks the code of the key), cool
         addItem();
     if (e.key == 'Delete')
         removeItem();
